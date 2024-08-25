2b711b0e8926cc3887327d070f8a798d898976c5
coder-sadhin <codersadhin@gmail.com>
2024-08-25 18:32:18 +0600

    node level

---
+++ node_level.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T val;
+        bool isOriginal;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
