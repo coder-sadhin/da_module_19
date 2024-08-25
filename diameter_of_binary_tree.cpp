1f7593d7f3f82f015a68f22e624da5f7d1d1b9fe
coder-sadhin <codersadhin@gmail.com>
2024-08-25 18:29:46 +0600

    diametter of binary tree

---
+++ diameter_of_binary_tree.cpp
@@ -0,0 +1,19 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
