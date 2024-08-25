afb92ddab74ab3b5fdd2752a12ea1afe3d9fb9d1
coder-sadhin <codersadhin@gmail.com>
2024-08-25 18:33:39 +0600

    binary tree

---
+++ special_binary_tree.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+/*************************************************************
+
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode
+    {
+    public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
