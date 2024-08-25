03e6b0b9f3db9637247b4e1485055bba6b9deda4
coder-sadhin <codersadhin@gmail.com>
2024-08-25 18:31:31 +0600

    left view

---
+++ left_view_of_a_binary_tree.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
